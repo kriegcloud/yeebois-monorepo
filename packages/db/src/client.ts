f863e662d81a568405f1d083d1aecac94b5f0b24
elpresidank <benjamin@krieg.cloud>
2024-11-17 02:53:54 -0600

    saving work

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,16 +1,39 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+
 import { Pool } from "pg";
+import schema from "./schema";
 
-import * as schema from "./schema";
+const connectionString = process.env.POSTGRES_URL;
 
+const pool = new Pool({ connectionString: connectionString });
 
-export { pgTable as tableCreator } from "./_table";
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-})
-
+export const db = drizzle(pool, {
+  schema,
+  logger: false,
+});
 export * from "drizzle-orm";
 
-export const db = drizzle(pool, { schema });
+export { alias } from "drizzle-orm/pg-core";
+
+// export function authDB<T>(
+//   session: Session,
+//   cb: (sql: NodePgDatabase) => T | Promise<T>,
+// ): Promise<T> {
+//   // You can add a validation here for the accessToken - we rely on supabase for now
+//   const jwtClaim = jose.decodeJwt(session.access_token);
+//   const role = jwtClaim.role as string;
+//
+//   return db.transaction(async (tx) => {
+//     // Set JWT to enable RLS. supabase adds the role and the userId (sub) to the jwt claims
+//     await tx.execute(
+//       sql`SELECT set_config('request.jwt.claims', '${sql.raw(
+//         JSON.stringify(jwtClaim),
+//       )}', TRUE)`,
+//     );
+//
+//     // do not use postgres because it will bypass the RLS, set role to authenticated
+//     await tx.execute(sql`set role '${sql.raw(role)}'`);
+//
+//     return cb(tx as any);
+//   }) as Promise<T>;
+// }
---
+++ packages/db/src/migrate.ts
@@ -0,0 +1,16 @@
+
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+
+import { db } from "./index";
+
+async function main() {
+  try {
+    await migrate(db, { migrationsFolder: "./drizzle" });
+    console.log("Migration complete");
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+main();
