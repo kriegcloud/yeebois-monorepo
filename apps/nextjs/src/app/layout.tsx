c60c6d298c9336f6940aa5e362257acb9aecf25b
elpresidank <benjamin@krieg.cloud>
2024-11-16 20:57:44 -0600

    first commit

---
+++ apps/nextjs/src/app/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata, Viewport } from "next";
+
+import { TRPCReactProvider } from "~/trpc/react";
+
+import "~/app/globals.css";
+
+import { env } from "~/env";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(
+    env.VERCEL_ENV === "production"
+      ? "https://turbo.t3.gg"
+      : "http://localhost:3000",
+  ),
+  title: "Create T3 Turbo",
+  description: "Simple monorepo with shared backend for web & mobile apps",
+  openGraph: {
+    title: "Create T3 Turbo",
+    description: "Simple monorepo with shared backend for web & mobile apps",
+    url: "https://create-t3-turbo.vercel.app",
+    siteName: "Create T3 Turbo",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@jullerino",
+    creator: "@jullerino",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default function RootLayout(props: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body>
+          <TRPCReactProvider>{props.children}</TRPCReactProvider>
+          <div className="absolute bottom-4 right-4">
+          </div>
+      </body>
+    </html>
+  );
+}
